50b4713108ccf30aa338b3e04215856f6b706ac1
Sachin <sachinsunny303@gmail.com>
2023-07-17 19:06:26 +0100

    server

--- 01intro/myInterface.ts
+++ 01intro/myInterface.ts
@@ -7,6 +7,25 @@ interface User {
   getDiscount: (discountType: string) => number;
 }
 
+interface Admin extends User {
+  adminId: number;
+  domainName: string;
+}
+
+const AdminDetails: Admin = {
+  dbId: 123,
+  email: 's@s.com',
+  userId: 456,
+  startTrial() {
+    return 'true';
+  },
+  getDiscount: (discountType = 'student') => {
+    return 10;
+  },
+  adminId: 123,
+  domainName: 'google.com',
+};
+
 const sachin: User = {
   dbId: 123,
   email: 's@s.com',
@@ -15,6 +34,6 @@ const sachin: User = {
     return 'true';
   },
   getDiscount: (discountType = 'student') => {
-    return '10';
+    return 10;
   },
 };
